fbf194c1ebefc322148a8857fdf5abf92b66b5a0
Arnar Snæland <addisnae@gmail.com>
2020-02-15 17:20:49 +0000

    mayebwork

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Login from "./components/login.component";
-import SignUp from "./components/signup.component";
+import Main from "./components/main.component";
 
 function App() {
   return (
@@ -15,13 +15,13 @@ function App() {
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
-                  <Link className="nav-link" to={"/sign-in"}>
+                  {/* <Link className="nav-link" to={"/sign-in"}>
                     Login
-                  </Link>
+                  </Link> */}
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to={"/sign-up"}>
-                    Sign up
+                  <Link className="nav-link" to={"/main"}>
+                    Main
                   </Link>
                 </li>
               </ul>
@@ -34,7 +34,7 @@ function App() {
             <Switch>
               <Route exact path="/" component={Login} />
               <Route path="/sign-in" component={Login} />
-              <Route path="/sign-up" component={SignUp} />
+              <Route path="/main" component={Main} />
             </Switch>
           </div>
         </div>
