9d014243f4bac57e16a4d5f4b3df89a4762b8acd
Arnar Snæland <addisnae@gmail.com>
2020-02-15 18:17:52 +0000

    fhsjkd

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = { PW: "" };
+    this.state = { Number: "" };
     this.handleChangeNum = this.handleChangeNum.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -17,11 +17,13 @@ export default class Login extends Component {
     var xhr = new XMLHttpRequest();
 
     console.log(this.state);
-    xhr.open("POST", "http://localhost:8084");
+    xhr.open("POST", "https://localhost:8084/loginForm");
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhr.send(JSON.stringify(this.state));
     this.setState = { Number: "" };
+
     xhr.addEventListener("load", () => {
+      console.log(xhr.status);
       if (xhr.status === 200) {
         this.props.history.push("/main");
       }
